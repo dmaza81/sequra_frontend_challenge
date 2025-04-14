ddd4cce1f26294ae150a115051abbb79b52174c4
Daniel Maza Arredondo <dmaza@mac.lan>
2025-04-14 16:26:36 +0200

    test: integrate de widget with an external page via CDN (Github pages)

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const renderWidget = (containerId: string, number: number) => {
-  const container = document.getElementById(containerId);
-  if (container) {
-    const root = ReactDOM.createRoot(container);
-    root.render(<App totalWithTax={number} />);
-  } else {
-    console.error(`No se encontró el contenedor con id: ${containerId}`);
-  }
+const renderWidget = (contaninerId: string, number: number) => {
+  const container = document.getElementById(contaninerId);
+  const root = ReactDOM.createRoot(container as HTMLElement);
+  root.render(<App totalWithTax={number} />);
 };
 
-// Exportar la función para que pueda ser utilizada en el código de inserción
-export { renderWidget };
+//@ts-expect-error winwo
+window.renderMyWidget = renderWidget;
